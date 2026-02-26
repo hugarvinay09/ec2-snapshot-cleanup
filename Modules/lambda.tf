cb777ce3c63ac781c8d85bcb2fc8bc070d4c26f2
Vinay Hugar <hugarvinay09@gmail.com>
2026-02-26 15:53:30 +0530

    modifed lambda function

--- Modules/lambda.tf
+++ Modules/lambda.tf
@@ -1,134 +1,102 @@
-########################################
-# Archive Lambda Code
-########################################
+###############################################################
+# Lambda Function for EC2 & Snapshot Cleanup
+###############################################################
 
-data "archive_file" "ec2_cleanup_zip" {
-  type        = "zip"
-  source_file = "${path.module}/lambda/ec2_cleanup.py"
-  output_path = "${path.module}/lambda/ec2_cleanup.zip"
+variable "project" {}
+variable "environment" {}
+variable "memory_size" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "security_groups" {
+  type = list(string)
+}
+variable "sns_topic_arn" {}
+variable "retention_days" {
+  default = 365
+}
+variable "dry_run" {
+  default = true
 }
 
-########################################
 # IAM Role for Lambda
-########################################
+resource "aws_iam_role" "lambda_exec_role" {
+  name               = "${var.project}-${var.environment}-lambda-role"
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+}
 
-resource "aws_iam_role" "lambda_role" {
-  name = "${var.project_name}-${var.environment}-lambda-role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Effect = "Allow",
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        },
-        Action = "sts:AssumeRole"
-      }
-    ]
-  })
-
-  tags = {
-    Environment = var.environment
-    Project     = var.project_name
+data "aws_iam_policy_document" "lambda_assume_role" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
   }
 }
 
-########################################
-# IAM Policy for EC2 + SNS Access
-########################################
-
-resource "aws_iam_role_policy" "lambda_policy" {
-  name = "${var.project_name}-${var.environment}-lambda-policy"
-  role = aws_iam_role.lambda_role.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-
-      # EC2 permissions
-      {
-        Effect = "Allow",
-        Action = [
-          "ec2:DescribeInstances",
-          "ec2:TerminateInstances",
-          "ec2:DescribeSnapshots",
-          "ec2:DeleteSnapshot"
-        ],
-        Resource = "*"
-      },
-
-      # SNS publish
-      {
-        Effect = "Allow",
-        Action = "sns:Publish",
-        Resource = aws_sns_topic.ec2_cleanup_topic.arn
-      },
-
-      # CloudWatch Logs
-      {
-        Effect = "Allow",
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Resource = "*"
-      }
-    ]
-  })
+# Attach Managed Policies
+resource "aws_iam_role_policy_attachment" "lambda_basic_exec" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-########################################
+resource "aws_iam_role_policy_attachment" "lambda_ec2_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_sns_policy" {
+  role       = aws_iam_role.lambda_exec_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSNSFullAccess"
+}
+
 # Lambda Function
-########################################
+resource "aws_lambda_function" "ec2_snapshot_cleanup" {
+  function_name = "${var.project}-${var.environment}-ec2-snapshot-cleanup"
+  role          = aws_iam_role.lambda_exec_role.arn
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.11"
+  memory_size   = var.memory_size
+  timeout       = 900  # 15 min max
 
-resource "aws_lambda_function" "ec2_cleanup" {
-  function_name = "${var.project_name}-${var.environment}-ec2-cleanup"
+  filename      = "lambda.zip"  # Build zip with deployment package
 
-  filename         = data.archive_file.ec2_cleanup_zip.output_path
-  source_code_hash = data.archive_file.ec2_cleanup_zip.output_base64sha256
-  handler          = "ec2_cleanup.lambda_handler"
-  runtime          = "python3.11"
-
-  role = aws_iam_role.lambda_execution_role.arn
-  timeout = 300
-  memory_size = 256
+  vpc_config {
+    subnet_ids         = var.subnet_ids
+    security_group_ids = var.security_groups
+  }
 
   environment {
     variables = {
-      SNS_TOPIC_ARN = aws_sns_topic.ec2_cleanup_topic.arn
-      RETENTION_DAYS = "365"
-      ENVIRONMENT    = var.environment
+      RETENTION_DAYS  = var.retention_days
+      DRY_RUN         = var.dry_run
+      SNS_TOPIC_ARN   = var.sns_topic_arn
+      ENVIRONMENT     = var.environment
     }
   }
 
-  tags = {
-    Name        = "${var.project_name}-${var.environment}-ec2-cleanup"
-    Environment = var.environment
-    Project     = var.project_name
-  }
+  depends_on = [aws_iam_role_policy_attachment.lambda_basic_exec]
 }
 
-########################################
-# CloudWatch EventBridge Schedule (Daily)
-########################################
-
-resource "aws_cloudwatch_event_rule" "daily_trigger" {
-  name                = "${var.project_name}-${var.environment}-daily-ec2-cleanup"
+# EventBridge Rule (Daily Trigger)
+resource "aws_cloudwatch_event_rule" "daily_lambda_trigger" {
+  name                = "${var.project}-${var.environment}-daily-lambda-trigger"
   schedule_expression = "rate(1 day)"
 }
 
+# EventBridge Target
 resource "aws_cloudwatch_event_target" "lambda_target" {
-  rule      = aws_cloudwatch_event_rule.daily_trigger.name
-  target_id = "LambdaTarget"
-  arn       = aws_lambda_function.ec2_cleanup.arn
+  rule      = aws_cloudwatch_event_rule.daily_lambda_trigger.name
+  target_id = "ec2-snapshot-cleanup"
+  arn       = aws_lambda_function.ec2_snapshot_cleanup.arn
 }
 
+# Lambda Permission for EventBridge
 resource "aws_lambda_permission" "allow_eventbridge" {
   statement_id  = "AllowExecutionFromEventBridge"
   action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.ec2_cleanup.function_name
+  function_name = aws_lambda_function.ec2_snapshot_cleanup.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.daily_trigger.arn
+  source_arn    = aws_cloudwatch_event_rule.daily_lambda_trigger.arn
 }
